d5bdf118dd52fe51a76df1b1037bd271df7767d6
diegoxgarcia <diegoxgarcia@gmail.com>
2024-09-17 19:21:26 -0300

    Update path.gd

--- Scenes/Levels/path.gd
+++ Scenes/Levels/path.gd
@@ -8,9 +8,10 @@ func _on_timer_timeout():
 	
 func _spawn_enemy():
 	var enemy : Enemy = enemy_scene.instantiate()
+	enemy.speed = randf_range(200.0, 400.0)
 	add_child(enemy)
 	randomize()
-	var rand_time = randf_range(2.0,8.0)
+	var rand_time = randf_range(1.0,2.0)
 	$Timer.wait_time = rand_time
 	$Timer.start()
 	pass
